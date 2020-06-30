c2635940ccc0c9f95d29c84c9e5d7be1f5a8a347
Roman Storm <roman@peppersec.com>
2020-06-30 16:17:20 -0700

    remove Math.sol

--- contracts/wNXM.sol
+++ contracts/wNXM.sol
@@ -3,7 +3,6 @@ pragma solidity 0.5.17;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
-import "@openzeppelin/contracts/math/Math.sol";
 import "./INXM.sol";
 import "./ERC20Permit.sol";
 
